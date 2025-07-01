3a08f8ef40c0913133fd20969c9b3819c11502d2
Alan Hassan <163721484+alanhasn@users.noreply.github.com>
2025-07-01 13:23:06 +0300

    implement pagenation on API

--- project/settings.py
+++ project/settings.py
@@ -148,7 +148,12 @@ REST_FRAMEWORK = {
         'list_tasks': '60/hour',
         'todo_edit': '30/hour',
         'user_info': '20/hour',
-    }
+    },
+
+    # pagenation settings
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+
 
 }
 
